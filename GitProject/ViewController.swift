4254323f3e5f984c81442ebd66c6faa4c20b4fbd
Sanket Hadke <sanket.hadke@maximess.com>
2019-10-09 14:44:08 +0530

    Add My name

--- GitProject/ViewController.swift
+++ GitProject/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var name  = "Sanket"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
